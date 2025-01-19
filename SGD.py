4724ac3896d8423f611f4f098ef62e90e2d8adaa
Vrishank <imvrishank@gmail.com>
2025-01-19 22:14:22 +0530

    fixed it
    It is fully functional. i have a bunch of random test code that ive written along with the SGD function, will clear it up later

--- SGD.py
+++ SGD.py
@@ -1,45 +1,76 @@
-#this is a simple implementation of the stochastic gradient descent algorithm
+#this is a simple implementation of the stochastic gradient descent algorithm from scratch
 
-
-
-#dataset is made global, we have only training data in dataset and we also seperated the dataset into features and labels
-#features if going to be a 2D array of m x n where m is the number of training examples and n is the number of features
 import numpy as np
 
 
 # for testing purposes i am going to use a paramaters numpy array as such:
 class test1:
     def __init__(self):
-        self.parameters = np.array([[0.1,0.2,0.3]])
-        self.n = 3
-        self.features = np.array([[1200,1,1],
-                    [1500,2,0],
-                    [1700,5,0],
-                    [1100,3,1]])
+        self.parameters = np.array([[0.0],
+                                    [0.0],
+                                    [0.0],
+                                    [0.0]])
+        self.n = 4
+        self.features = np.array([
+                    [1200,1,1,2],
+                    [1500,2,0,2],
+                    [1700,5,0,4],
+                    [1100,3,1,6],
+                    [1300,3,0,2]])
         
-        self.training_examples = 4
+        self.training_examples = 5
 
         self.lables  = np.array([[100],
                             [200],
                             [300],
-                            [400]])
+                            [400],
+                            [300]])
         
 
 
 
 
-def SDG(learning_rate, parameters, features, lables, training_examples):
-    training_examples = features.shape[0]
-    for parameter in parameters:
-        for i in range(0,training_examples):
-            #grabbing the array of featurs for the ith training example
-            x_i = features[i,:]
-            parameters_i = parameters #for simplicity i am assuming that parameters are a n x 1 array
-            hypothesis_i = np.cross(x_i,parameters_i)
-            parameter = parameter - learning_rate*((hypothesis_i - lables[i][0])[0][0])*x_i[i]
+def SGD(learning_rate, parameters, features, lables): 
 
+    training_examples = features.shape[0]
+
+    #iterating through the parameters
+    for j in range(0,parameters.shape[0]):
+        #grabbing the perticular parameter we are going to be altering in the for loop
+        parameter = parameters[j][0]
+
+        #iterating through the training examples
+        for i in range(0,training_examples):
+            #grabbing the array of features for the ith training example
+            x_i = features[i,:].reshape(-1,features.shape[1])
+
+            hypothesis_i = np.dot(x_i,parameters)[0][0]
+
+            lable_i = lables[i][0]
+
+            x_j = x_i[0][j]
+
+            #updating the parameter
+            parameter = parameter - learning_rate*(hypothesis_i - lable_i)*x_j
+
+            #uploading the changed parameter to the parameter matrix
+            
+            parameters[j][0] = parameter
+
+        
     return parameters
+
+
+
+
+
+            
+
+
+
+        
+
 
 test = test1()
 
-print(SDG(learning_rate = 0.01,parameters = test.parameters, features = test.features, lables = test.lables ,training_examples = test.training_examples))
+print(SGD(learning_rate = 0.01,parameters = test.parameters, features = test.features, lables = test.lables))
